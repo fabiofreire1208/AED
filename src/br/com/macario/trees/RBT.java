0b633921562cd39a36f2fb89210b6d03b84dd079
fabiofreire1208 <fabiofreire1208@gmail.com>
2016-11-13 17:19:00 -0300

    Inserindo metodo de insertFixUp na arvore rubro negra

--- src/br/com/macario/trees/RBT.java
+++ src/br/com/macario/trees/RBT.java
@@ -1,6 +1,7 @@
 package br.com.macario.trees;
 
 import br.com.macario.aed.nodes.Node;
+import br.com.macario.aed.nodes.NodeRBT;
 
 /**
  * Classe que implementa os m�todos de uma �rvore Rubro Negra
@@ -54,13 +55,13 @@ public class RBT extends BST{
 		Node y = new Node();
 		y = node.getSubTreeLeft();
 		node.setSubTreeLeft(y.getSubTreeRight());
-		
+
 		if(y.getSubTreeRight() != null){
 			y.getSubTreeRight().setFather(node);
 		}
-		
+
 		y.setFather(node.getFather());
-		
+
 		if(node.getFather() == null){
 			setRoot(y);
 		} else if(node == node.getFather().getSubTreeRight()){
@@ -68,9 +69,68 @@ public class RBT extends BST{
 		} else {
 			node.getFather().setSubTreeLeft(y);
 		}
-		
+
 		y.setSubTreeRight(node);
 		node.setFather(y);
 	}
 
+	public void insertFixUp(Node node){
+		while(((NodeRBT) node.getFather()).getColor() == NodeRBT.RED){
+
+			if(node.getFather() == node.getFather().getFather().getSubTreeLeft()){
+				NodeRBT y = new NodeRBT();
+				y = (NodeRBT) node.getFather().getFather().getSubTreeRight();
+
+				//Caso 1
+				if(y.getColor() == NodeRBT.RED){
+					((NodeRBT) node.getFather()).setColor(NodeRBT.BLACK);
+					y.setColor(NodeRBT.BLACK);
+					((NodeRBT) node.getFather().getFather()).setColor(NodeRBT.RED);
+					node = node.getFather().getFather();
+
+					//caso 2
+				} else if(node == node.getFather().getSubTreeRight()){
+					node = node.getFather();
+					leftRotate(node);
+				}
+
+				//caso 3
+				else {
+					((NodeRBT) node.getFather()).setColor(NodeRBT.BLACK);
+					((NodeRBT) node.getFather().getFather()).setColor(NodeRBT.RED);
+					rightRotate(node.getFather().getFather());
+				}
+
+
+			} else {
+			
+				NodeRBT y = new NodeRBT();
+				y = (NodeRBT) node.getFather().getFather().getSubTreeLeft();
+
+				//caso 1
+				if (y.getColor() == NodeRBT.RED){
+					((NodeRBT) node.getFather()).setColor(NodeRBT.BLACK);
+					y.setColor(NodeRBT.BLACK);
+					((NodeRBT) node.getFather().getFather()).setColor(NodeRBT.RED);
+					node = node.getFather().getFather();
+				}
+
+				//caso 2
+				else if (node == node.getFather().getSubTreeLeft()){
+					node = node.getFather();
+					rightRotate(node);
+				}
+				//caso 3
+				else{
+					((NodeRBT) node.getFather()).setColor(NodeRBT.BLACK);
+					((NodeRBT) node.getFather().getFather()).setColor(NodeRBT.RED);
+					leftRotate(node.getFather().getFather());
+				}
+			}
+		}
+
+
+		((NodeRBT) getRoot()).setColor(NodeRBT.BLACK);
+
+	}
 }
